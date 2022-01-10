66af0a4424a436c35250f4c15de2021ed2236950
Ahmed Damasy <ahmeddamasy93@gmail.com>
2022-01-11 00:45:10 +0200

    get all users

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
 import './App.css';
+import CreateUser from './Components/CreateUser';
+import ListOfUsers from './Components/ListOfUsers';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql',
+  cache: new InMemoryCache()
+});
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ApolloProvider client={client}>
+        <CreateUser/>
+        <ListOfUsers/>
+      </ApolloProvider>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ client/src/Components/ListOfUsers.tsx
@@ -0,0 +1,41 @@
+import { useQuery } from '@apollo/client'
+import React from 'react'
+import { GET_ALL_USERS } from '../Graphql/Quaries'
+
+function ListOfUsers() {
+  const {data, error} = useQuery(GET_ALL_USERS)
+  console.log(data)
+  if(error) return (<h1>Error Happened!</h1>)
+
+  return (
+    <div>
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>name</th>
+              <th>username</th>
+              <th>password</th>
+              <th>actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data && data.getAllUsers.map((user: any) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.password}</td>
+                <td>
+                  <button className='btn btn-danger'>Delete</button>
+                  <button className='btn btn-primary'>Change Password</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default ListOfUsers
